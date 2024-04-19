f6ed5392f712d8c686fb93a56d115b9637c198e2
Faeest <faeest.115@gmail.com>
2024-04-19 12:36:31 +0700

    feat: Add 3xl breakpoint in Tailwind CSS and make dashboard and drawer responsive
    Description:
    - Added a new breakpoint named '3xl' for 1920px in Tailwind CSS configuration.
    - Updated dashboard and drawer/sidebar components to be responsive.
    - Ensured the dashboard and drawer/sidebar adapt to different screen sizes, including the new 3xl breakpoint.
    - Implemented functionality to minimize the drawer/sidebar when the screen size is lg breakpoint or larger.

--- resources/views/admin/layouts/partials/nav.blade.php
+++ resources/views/admin/layouts/partials/nav.blade.php
@@ -1,16 +1,8 @@
 <div
-    class="w-full transition-all z-[1000] outline outline-1 shadow outline-base-content/20 navbar justify-center lg:px-5 md:px-10 sticky top-0 bg-base-100/75 backdrop-blur-lg">
-    <div class="max-lg:container lg:w-full lg:pe-10 flex justify-between">
-        <div class="text-xl btn btn-ghost no-animation rounded-sm px-5">Dashboard</div>
+    class="w-full transition-all z-[1000] border-b-[1px] border-base-content/20 shadow navbar justify-center lg:px-5 md:px-10 sticky top-0 bg-base-100/75 backdrop-blur-lg">
+    <div class="w-full lg:pe-10 flex justify-between">
+        <div class="text-xl font-semibold select-none rounded-sm px-5 max-lg:ps-0">Dashboard</div>
         <div class="items-center flex gap-2 grow justify-end">
-            <a aria-label="nav-link" href="#beranda" class="nav-links max-lg:hidden"><span
-                    class="text-2xl icon-[ic--round-home]"></span>Beranda</a>
-            <a aria-label="nav-link" href="#section2" class="nav-links max-lg:hidden"><span
-                    class="text-2xl icon-[iconoir--book-solid]"></span>section2</a>
-            <a aria-label="nav-link" href="#section3" class="nav-links max-lg:hidden"><span
-                    class="text-2xl icon-[ic--person]"></span>section3</a>
-            <a aria-label="nav-link" href="#section4" class="nav-links max-lg:hidden"><span
-                    class="text-2xl icon-[ic--person]"></span>section4</a>
             <label for="sidebar-nav" aria-label="open sidebar" class="btn btn-circle btn-ghost lg:hidden">
                 <span class="icon-[heroicons-solid--menu-alt-3] text-2xl"></span>
             </label>
